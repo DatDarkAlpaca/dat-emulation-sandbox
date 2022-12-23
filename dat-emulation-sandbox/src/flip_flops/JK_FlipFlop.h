486e616bc4c99c8ea94bad254d373edfd307ba2a
Alpaca <paulocastrogalvao@hotmail.com>
2022-12-22 20:47:02 -0400

    Fix name errors

--- dat-emulation-sandbox/src/flip_flops/JK_FlipFlop.h
+++ dat-emulation-sandbox/src/flip_flops/JK_FlipFlop.h
@@ -22,8 +22,8 @@ namespace dat
 	public:
 		JK_FlipFlop()
 		{
-			nandGateIn[0][NAND_Gate_4IN::IN_0] = OFF;
-			nandGateIn[1][NAND_Gate_4IN::IN_2] = ON;
+			nandGateIn[0][NAND_Gate_3IN::IN_0] = OFF;
+			nandGateIn[1][NAND_Gate_3IN::IN_2] = ON;
 			process();
 		}
 
@@ -31,21 +31,21 @@ namespace dat
 		void process() override
 		{
 			//nandGateIn[0][NAND_Gate_4IN::IN_0] = default;
-			nandGateIn[0][NAND_Gate_4IN::IN_1] = getPin(J);
-			nandGateIn[0][NAND_Gate_4IN::IN_2] = getPin(CLOCK);
+			nandGateIn[0][NAND_Gate_3IN::IN_1] = getPin(J);
+			nandGateIn[0][NAND_Gate_3IN::IN_2] = getPin(CLOCK);
 			nandGateIn[0].process();
 
-			nandGateIn[1][NAND_Gate_4IN::IN_0] = getPin(CLOCK);
-			nandGateIn[1][NAND_Gate_4IN::IN_1] = getPin(K);
+			nandGateIn[1][NAND_Gate_3IN::IN_0] = getPin(CLOCK);
+			nandGateIn[1][NAND_Gate_3IN::IN_1] = getPin(K);
 			//nandGateIn[1][NAND_Gate_4IN::IN_2] = default;
 			nandGateIn[1].process();
 
-			nandGateOut[0][NAND_Gate::IN_0] = nandGateIn[0][NAND_Gate_4IN::OUT];
+			nandGateOut[0][NAND_Gate::IN_0] = nandGateIn[0][NAND_Gate_3IN::OUT];
 			nandGateOut[0][NAND_Gate::IN_1] = nandGateOut[1][NAND_Gate::OUT];
 			nandGateOut[0].process();
 			
 			nandGateOut[1][NAND_Gate::IN_0] = nandGateOut[0][NAND_Gate::OUT];
-			nandGateOut[1][NAND_Gate::IN_1] = nandGateIn[1][NAND_Gate_4IN::OUT];
+			nandGateOut[1][NAND_Gate::IN_1] = nandGateIn[1][NAND_Gate_3IN::OUT];
 			nandGateOut[1].process();
 
 			nandGateOut[0][NAND_Gate::IN_1] = nandGateOut[1][NAND_Gate::OUT];
@@ -54,12 +54,12 @@ namespace dat
 			setPin(Q, nandGateOut[0][NAND_Gate::OUT]);
 			setPin(Q_INV, nandGateOut[1][NAND_Gate::OUT]);
 
-			nandGateIn[0][NAND_Gate_4IN::IN_0] = nandGateIn[1][NAND_Gate::OUT];
-			nandGateIn[1][NAND_Gate_4IN::IN_2] = nandGateIn[0][NAND_Gate::OUT];
+			nandGateIn[0][NAND_Gate_3IN::IN_0] = nandGateIn[1][NAND_Gate::OUT];
+			nandGateIn[1][NAND_Gate_3IN::IN_2] = nandGateIn[0][NAND_Gate::OUT];
 		}
 
 	private:
-		std::array<NAND_Gate_4IN, 2> nandGateIn;
+		std::array<NAND_Gate_3IN, 2> nandGateIn;
 		std::array<NAND_Gate, 2> nandGateOut;
 	};
 }
